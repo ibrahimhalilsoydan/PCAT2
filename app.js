e1f505d5c4cca5eb1636bfcf4ffacbede3f8be90
ibrahim halil SOYDAN <ibrahimhalilsoydan@gmail.com>
2025-06-11 13:57:55 +0300

    Static files upleaded

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
-const fn = function () {
-  console.log('Function working');
-};
+import express from 'express';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`sunucu ${port} portunda çalışıyor`);
+});
